4d7b38acdaa18affdba0dffcdb4197f5d0e3c985
Madeleine Forgeron <99274168+MadeleineForgeron@users.noreply.github.com>
2022-03-23 18:21:57 +0000

    now do css

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-// Copyright (c) 2022 Ms Raffin All rights reserved
+// Copyright (c) 2022 Madeleine Forgeron All rights reserved
 //
-// Created by: Ms Raffin
-// Created on: Sep 2022
+// Created by: Madeleine Forgeron
+// Created on: March 2022
 // This file contains the JS functions for index.html
 
 function enterClicked () {
-  // this function gets user's first name and Street Number and displays it back to user
+  // this function gets user's street name and Street Number and displays it back to user
 
-  // get first name from text field
-  let firstName = document.getElementById("first-name").value
+  // get street name from text field
+  let streetName = document.getElementById("street-name").value
 	// get Street Number from text field and cast it to integer
   let userStreetNumber = parseInt(document.getElementById("streetnumber-entered").value)
 
-  // display name and Street Number back to user
-  document.getElementById('user-info').innerHTML = "Your first name is " + firstName + " and you are " + userStreetNumber + " years old."
+  // display Street Name and Street Number back to user
+  document.getElementById('user-info').innerHTML = "Your street name is " + streetName + " and your street number is " + userStreetNumber + "."
 }
