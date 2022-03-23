b7544e60992f541213d091b60b48e7bd6b4f3e51
Madeleine Forgeron <99274168+MadeleineForgeron@users.noreply.github.com>
2022-03-23 15:06:03 +0000

    I lied do change to street name and number

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@
 // This file contains the JS functions for index.html
 
 function enterClicked () {
-  // this function gets user's first name and age and displays it back to user
+  // this function gets user's first name and Street Number and displays it back to user
 
   // get first name from text field
   let firstName = document.getElementById("first-name").value
-	// get age from text field and cast it to integer
-  let userAge = parseInt(document.getElementById("age-entered").value)
+	// get Street Number from text field and cast it to integer
+  let userStreetNumber = parseInt(document.getElementById("streetnumber-entered").value)
 
-  // display name and age back to user
-  document.getElementById('user-info').innerHTML = "Your first name is " + firstName + " and you are " + userAge + " years old."
+  // display name and Street Number back to user
+  document.getElementById('user-info').innerHTML = "Your first name is " + firstName + " and you are " + userStreetNumber + " years old."
 }
